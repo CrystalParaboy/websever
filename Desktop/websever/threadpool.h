7e4ae1c2630be19d02d7c5844c959f1a3df8ae6d
CrystalParaboy <1970945942@qq.com>
2022-11-24 22:18:07 +0800

    up threadpool

--- Desktop/websever/threadpool.h
+++ Desktop/websever/threadpool.h
@@ -14,7 +14,7 @@ public:
     /*thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
     threadpool(int thread_number=8, int max_request=10000);
     ~threadpool();
-    bool append(T *request, int state);
+    bool append(T *request);
     bool append_p(T *request);
 private:
     static void *worker(void *arg);
@@ -34,6 +34,73 @@ threadpool<T>::threadpool(int threadpool_number, int max_requests):
                 m_thread_number(thread_number),m_max_requests(max_requests),
                                 m_stop(false),m_threads(NULL)
 {
-    
+    if((thread_number<=0)||(max_requests<=0)){
+        throw std::exception();
+    }
+    m_threads = new pthread_t[m_thread_number];
+    if (!m_threads)
+        throw std::exception();
+    //create thread_number xiancheng and set them detach.
+    for (int i = 0; i < thread_number; ++i)
+    {
+        if (pthread_create(m_threads + i, NULL, worker, this) != 0)
+        {
+            delete[] m_threads;
+            throw std::exception();
+        }
+        if (pthread_detach(m_threads[i]))
+        {
+            delete[] m_threads;
+            throw std::exception();
+        }
+    }
 }
+template <typename T>
+threadpool<T>::~threadpool()
+{
+    delete[] m_threads;
+    m_stop=true;
+}
+
 /*add work to request quene*/
+template <typename T>
+bool threadpool<T>::append(T *request)
+{
+    m_queuelocker.lock();
+    if (m_workqueue.size() >= m_max_requests)
+    {
+        m_queuelocker.unlock();
+        return false;
+    }
+    m_workqueue.push_back(request);
+    m_queuelocker.unlock();
+    m_queuestat.post();
+    return true;
+}
+template <typename T>
+void* threadpool<T>::worker(void *arg){
+    threadpool *pool = (threadpool *)arg;
+    pool->run();
+    return pool;
+}
+
+
+template <typename T>
+void threadpool<T>::run(){
+    while(!m_stop){
+        m_queuestat.wait();
+        m_queuelocker.lock();
+        if(m_workqueue.empty()){
+            m_queuelocker.unlock();
+            continue;
+        }
+        T* request=m_workqueue.front();
+        m_workqueue.pop_front;
+        m_queuelocker.unlock();
+        if(!request){
+            continue;
+        }
+        request->process();
+    }
+}
+#endif
