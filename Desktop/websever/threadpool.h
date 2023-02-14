fdf4b5f033eac92a31a77940af201adfc7951c64
Sakamoto <88869510+CrystalParaboy@users.noreply.github.com>
2023-02-14 22:49:54 +0800

    Update threadpool.h

--- Desktop/websever/threadpool.h
+++ Desktop/websever/threadpool.h
@@ -2,105 +2,65 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
-#include <list>
-#include <cstdio>
-#include <exception>
-#include <pthread.h>
-#include "locker.h"
-
-template<typename T>
-class threadpool{
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <thread>
+#include <functional>
+class ThreadPool {
 public:
-    /*thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
-    threadpool(int thread_number=8, int max_request=10000);
-    ~threadpool();
-    bool append(T *request);
-    bool append_p(T *request);
-private:
-    static void *worker(void *arg);
-    void run();
+    explicit ThreadPool(size_t threadCount = 8): pool_(std::make_shared<Pool>()) {
+            assert(threadCount > 0);
+            for(size_t i = 0; i < threadCount; i++) {
+                std::thread([pool = pool_] {
+                    std::unique_lock<std::mutex> locker(pool->mtx);
+                    while(true) {
+                        if(!pool->tasks.empty()) {
+                            auto task = std::move(pool->tasks.front());
+                            pool->tasks.pop();
+                            locker.unlock();
+                            task();
+                            locker.lock();
+                        } 
+                        else if(pool->isClosed) break;
+                        else pool->cond.wait(locker);
+                    }
+                }).detach();
+            }
+    }
 
-    int m_thread_number;        //线程池中的线程数
-    int m_max_requests;         //请求队列中允许的最大请求数
-    pthread_t *m_threads;       //描述线程池的数组，其大小为m_thread_number
-    std::list<T*> m_workqueue; //请求队列
-    locker m_queuelocker;       //保护请求队列的互斥锁
-    sem m_queuestat;            //是否有任务需要处理
-    bool m_stop;
+    ThreadPool() = default;
+
+    ThreadPool(ThreadPool&&) = default;
+    
+    ~ThreadPool() {
+        if(static_cast<bool>(pool_)) {
+            {
+                std::lock_guard<std::mutex> locker(pool_->mtx);
+                pool_->isClosed = true;
+            }
+            pool_->cond.notify_all();
+        }
+    }
+
+    template<class F>
+    void AddTask(F&& task) {
+        {
+            std::lock_guard<std::mutex> locker(pool_->mtx);
+            pool_->tasks.emplace(std::forward<F>(task));
+        }
+        pool_->cond.notify_one();
+    }
+
+private:
+    struct Pool {
+        std::mutex mtx;
+        std::condition_variable cond;
+        bool isClosed;
+        std::queue<std::function<void()>> tasks;
+    };
+    std::shared_ptr<Pool> pool_;
 };
 
-template<typename T>
-threadpool<T>::threadpool(int threadpool_number, int max_requests):
-                m_thread_number(thread_number),m_max_requests(max_requests),
-                                m_stop(false),m_threads(NULL)
-{
-    if((thread_number<=0)||(max_requests<=0)){
-        throw std::exception();
-    }
-    m_threads = new pthread_t[m_thread_number];
-    if (!m_threads)
-        throw std::exception();
-    //create thread_number xiancheng and set them detach.
-    for (int i = 0; i < thread_number; ++i)
-    {
-        if (pthread_create(m_threads + i, NULL, worker, this) != 0)
-        {
-            delete[] m_threads;
-            throw std::exception();
-        }
-        if (pthread_detach(m_threads[i]))
-        {
-            delete[] m_threads;
-            throw std::exception();
-        }
-    }
-}
-template <typename T>
-threadpool<T>::~threadpool()
-{
-    delete[] m_threads;
-    m_stop=true;
-}
 
-/*add work to request quene*/
-template <typename T>
-bool threadpool<T>::append(T *request)
-{
-    m_queuelocker.lock();
-    if (m_workqueue.size() >= m_max_requests)
-    {
-        m_queuelocker.unlock();
-        return false;
-    }
-    m_workqueue.push_back(request);
-    m_queuelocker.unlock();
-    m_queuestat.post();
-    return true;
-}
-template <typename T>
-void* threadpool<T>::worker(void *arg){
-    threadpool *pool = (threadpool *)arg;
-    pool->run();
-    return pool;
-}
-
-
-template <typename T>
-void threadpool<T>::run(){
-    while(!m_stop){
-        m_queuestat.wait();
-        m_queuelocker.lock();
-        if(m_workqueue.empty()){
-            m_queuelocker.unlock();
-            continue;
-        }
-        T* request=m_workqueue.front();
-        m_workqueue.pop_front;
-        m_queuelocker.unlock();
-        if(!request){
-            continue;
-        }
-        request->process();
-    }
-}
-#endif
+#endif //THREADPOOL_H
